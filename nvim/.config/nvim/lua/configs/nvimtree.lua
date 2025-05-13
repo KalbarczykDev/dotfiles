691eb1169e6b11831924f5a991ce53c4460baf9a
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-05-13 18:16:43 +0200

    configure nvimtree

--- nvim/.config/nvim/lua/configs/nvimtree.lua
+++ nvim/.config/nvim/lua/configs/nvimtree.lua
@@ -2,15 +2,36 @@ local M = {}
 
 function M.setup()
   local default = require "nvchad.configs.nvimtree"
+  local nvimtree = require "nvim-tree"
 
-  require("nvim-tree").setup(vim.tbl_deep_extend("force", default, {
+  local opts = {
     view = {
-      width = 40,
+      width = 50,
+      side = "left",
     },
     renderer = {
       highlight_git = true,
+      group_empty = true,
+      icons = {
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = true,
+        },
+      },
     },
-  }))
+    filters = {
+      dotfiles = false,
+      custom = { ".DS_Store" },
+    },
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+    },
+  }
+
+  nvimtree.setup(vim.tbl_deep_extend("force", default, opts))
 end
 
 M.setup()
